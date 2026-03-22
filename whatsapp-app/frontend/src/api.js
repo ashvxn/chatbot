1c8199c7b9fe71817f42c4e0ab54886ed9f9a17e
ashvin <ashvinpkumar2004@gmail.com>
2026-03-22 21:41:47 +0530

    some more changes

--- whatsapp-app/frontend/src/api.js
+++ whatsapp-app/frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Automatically detect the correct URL based on how you are accessing the page
 const isDev = window.location.port === "5173";
-const baseURL = isDev ? "http://localhost:5000" : window.location.origin;
+const baseURL = isDev ? "http://localhost:5000" : "https://jettie-indiscretionary-hilda.ngrok-free.dev";
 
 export default axios.create({
   baseURL: `${baseURL}/api`
